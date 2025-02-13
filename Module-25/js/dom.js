419bbcb0085e755b1872a25c5f1616ac7a2a9f18
Mosharof <md5470415@gmail.com>
2025-02-13 11:42:45 +0600

    dom concept rivision done

---
+++ Module-25/js/dom.js
@@ -0,0 +1,12 @@
+console.log(document)
+console.log('connected dom')
+
+// getElementsByTagNames()
+
+const liCollections = document.getElementsByTagName('li');
+console.log(liCollections.innerText)
+
+// getElementById()
+// getElementsByClassName()
+// querySelector()
+// querySelectorAll()
